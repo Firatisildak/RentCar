0187daa083d8c4d138fea14118087f081640501c
Fırat Işıldak <firatisildak1@gmail.com>
2023-12-29 11:51:13 +0000

    delete and update function writen add

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -16,8 +16,8 @@ namespace DataAccess.Concrete.InMemory
         {
             _car = new List<Car> {
                 new Car{Id=1, BrandId=1, ColorId=1, DailyPrice=100, ModelYear=2012, Description="uzun kasa"},
-                new Car{Id=2, BrandId=2, ColorId=2, DailyPrice=200, ModelYear=2023, Description="uzun kasa"},
-                new Car{Id=3, BrandId=3, ColorId=3, DailyPrice=300, ModelYear=2017, Description="uzun kasa"}
+                new Car{Id=2, BrandId=2, ColorId=2, DailyPrice=200, ModelYear=2023, Description="kısa kasa"},
+                new Car{Id=3, BrandId=3, ColorId=3, DailyPrice=300, ModelYear=2017, Description="kapalı kasa"}
             };
         }
 
@@ -28,7 +28,14 @@ namespace DataAccess.Concrete.InMemory
 
         public void Delete(Car car)
         {
-            throw new NotImplementedException();
+            Car? carToDelete=_car.SingleOrDefault(p=>p.Id==car.Id);
+            if (carToDelete != null)
+            {
+                _car.Remove(carToDelete);
+            }else
+            {
+                Console.WriteLine("Eksik veya Boş değer var.");
+            }
         }
 
         public List<Car> GetAll()
@@ -38,12 +45,24 @@ namespace DataAccess.Concrete.InMemory
 
         public List<Car> GetById(int brandId)
         {
-            throw new NotImplementedException();
+            return _car.Where(p=>p.BrandId==brandId).ToList();
         }
 
         public void Update(Car car)
         {
-            throw new NotImplementedException();
+            Car? carToUpdate = _car.SingleOrDefault(p => p.Id == car.Id);
+            if (carToUpdate != null)
+            {
+                carToUpdate.BrandId = car.BrandId;
+                carToUpdate.ColorId = car.ColorId;
+                carToUpdate.DailyPrice = car.DailyPrice;
+                carToUpdate.ModelYear = car.ModelYear;
+                carToUpdate.Description = car.Description;
+            }
+            else
+            {
+                Console.WriteLine("Eksik veya Boş değer var.");
+            }
         }
     }
 }
