354bf89e73e2d5a3e9d2da2b95dc9fc33b920e21
Fırat Işıldak <firatisildak1@gmail.com>
2023-12-31 14:18:51 +0000

    Business layer written and testing

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -11,8 +11,7 @@ namespace DataAccess.Concrete.InMemory
     public class InMemoryCarDal : ICarDal
     {
         List<Car> _car;
-
-        public InMemoryCarDal(List<Car> car)
+        public InMemoryCarDal()
         {
             _car = new List<Car> {
                 new Car{Id=1, BrandId=1, ColorId=1, DailyPrice=100, ModelYear=2012, Description="uzun kasa"},
