807fdfc60437f1a1e1f81ddecb67053116f6a676
Fırat Işıldak <firatisildak1@gmail.com>
2024-01-22 07:43:37 +0000

    Database add

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarServise
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            //İş kodları
+            //Yetkisi Var mı?
+            return _carDal.GetAll();
+        }
+    }
+}
