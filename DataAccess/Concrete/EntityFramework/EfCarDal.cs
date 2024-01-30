d197bdcb8b2255003b5cd8b4f4f138f1c8e8ee1d
Fırat Işıldak <firatisildak1@gmail.com>
2024-01-30 17:21:28 +0000

    Dto added

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,22 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, RenteDatabaseContext>, ICarDal
     {
-        
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (RenteDatabaseContext context= new RenteDatabaseContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands on c.BrandId equals b.BrandId
+                             join d in context.Colors on c.ColorId equals d.ColorId
+                             select new CarDetailDto
+                             {
+                                 CarName = c.CarName,
+                                 BrandName = b.BrandName,
+                                 ColorName = d.ColorName,
+                                 DailyPrice = c.DailyPrice
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
